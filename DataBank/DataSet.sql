ea5e4406beb53fbe1a6b921ec8d57baf596ce251
SungJi93 <131073857+SungJi93@users.noreply.github.com>
2023-05-04 19:56:35 -0400

    Update DataSet.sql
    T01.3 has been relocated to new file

--- DataBank/DataSet.sql
+++ DataBank/DataSet.sql
@@ -1,19 +1,5 @@
 -- DataBank - Last updated 04/19/2023 07:45PM EST
 
--- T01.3: insert data to customer
-INSERT INTO customer 
-	VALUES (seq_cid.nextval, 'Wendi Robles', '202 W. Shore Ave', 'OH', 43612, 'rattha@problemstory.us', 0);
-INSERT INTO customer 
-	VALUES (seq_cid.nextval, 'Darnell House', '7765 Taklin Hill Dr.', 'CA', 95301, 'fxllipek@mtcxmail.com', 350.00);
-INSERT INTO customer
-	VALUES (seq_cid.nextval, 'Korey Booth', '67 Sycamore St', 'GA', 31021, 'tavaira@kientao.online', 200.00);
-INSERT INTO customer
-	VALUES (seq_cid.nextval, 'Milly Jenkins', '13458 Centennial Lane', 'MD', 21042, 'milly@gmail.com', 15.00);
-INSERT INTO customer
-	VALUES (seq_cid.nextval, 'Sam Houston', '4277 Winsor mills rd.', 'MD', 22442, 'sam@gmail.com', 1.00);
-INSERT INTO customer
-	VALUES (seq_cid.nextval, 'Ben Franklin', '84 Milky way', 'MD', 21444, 'benf@gmail.com', 0);
-
 -- T02.3: insert date to discount
 INSERT INTO discount (did, discount_description, discount_type) 
 	VALUES (discount_seq.nextval, 'Free delivery', 1);
